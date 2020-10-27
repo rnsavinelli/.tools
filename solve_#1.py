f725f2819ab38d809de05b11d035af4ea9f9029b
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-10-27 17:52:44 -0300

    add solve_#1.py

---
+++ solve_#1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""
+ Copyright (c) 2020 Savinelli Roberto Nicolás <rsavinelli@est.frba.utn.edu.ar>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+"""
+
+from math import sqrt
+
+EPSILON = 1e-8
+
+def isPrime(n) :
+    if (n <= 1) :
+        return False
+    if (n <= 3) :
+        return True
+
+    if (n % 2 == 0 or n % 3 == 0) :
+        return False
+
+    i = 5
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
+            return False
+        i = i + 6
+
+    return True
+
+
+def almostEqual(a, b):
+    return abs(a - b) <= EPSILON
+
+def solve():
+    # If p = 1 then sqrt(2*p) does not belong to N
+    p = 3
+
+    while(True):
+        if (isPrime(p)):
+            sqrt2p = sqrt(2*p)
+            x = (sqrt2p - int(sqrt2p))
+            print("{}: {}".format(p, x))
+
+            if(almostEqual(x, 0.0)):
+                print("False: {}".format(p))
+                return 0
+
+        p=p+1
+
+if __name__ == '__main__':
+    import sys
+
+    solve()
